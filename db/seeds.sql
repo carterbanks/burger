6b5b2a387db32a683a9148c382427d337b169f15
carterbanks <carterbryantbanks@gmail.com>
2018-05-16 11:56:21 -0500

    Burger Commit

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+
+INSERT INTO burgers
+    (person_name, burger_name)
+VALUES
+    ('Francis','Single Cheeseburger');
+INSERT INTO burgers
+    (person_name, burger_name)
+VALUES
+    ('Jacob','Double Cheeseburger');
+INSERT INTO burgers
+    (person_name, burger_name)
+VALUES
+    ('Cynthia','Triple Cheeseburger');
